404bdf24fc6c3403bde34398f1e929e2f9b7713f
Arnold Cano <arnoldcano@yahoo.com>
2015-01-24 11:23:10 -0600

    Added code for button testing.

--- goncierge.go
+++ goncierge.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	"time"
+	//"time"
 
 	"github.com/hybridgroup/gobot"
+	"github.com/hybridgroup/gobot/api"
 	"github.com/hybridgroup/gobot/platforms/gpio"
 	"github.com/hybridgroup/gobot/platforms/raspi"
 )
@@ -12,29 +13,46 @@ import (
 func main() {
 	gbot := gobot.NewGobot()
 
-	//Use at your own risk! This is untested on pi
+	api.NewAPI(gbot).Start()
+
 	r := raspi.NewRaspiAdaptor("raspi")
-	pin := gpio.NewDirectPinDriver(r, "pin", "13")
+	//pin := gpio.NewDirectPinDriver(r, "pin", "13")
 	led := gpio.NewLedDriver(r, "led", "7")
+	button := gpio.NewButtonDriver(r, "button", "13")
 
 	work := func() {
-		gobot.Every(1*time.Second, func() {
-			v, err := pin.DigitalRead()
-			if err != nil {
-				fmt.Printf("Digital Read Error: %s", err.Error())
-			}
-			if v == 1 {
-				fmt.Printf("Motion Detected: %v\n", v)
-				led.On()
-			} else {
-				led.Off()
-			}
+		gobot.On(button.Event("push"), func(data interface{}) {
+			fmt.Println("Button Pushed")
+			led.On()
+		})
+		gobot.On(button.Event("release"), func(data interface{}) {
+			fmt.Println("Button Released")
+			led.Off()
 		})
 	}
 
+	/*
+		work := func() {
+			gobot.Every(500*time.Millisecond, func() {
+				v, err := pin.DigitalRead()
+				if err != nil {
+					fmt.Printf("Digital Read Error: %s\n", err.Error())
+				}
+				if v == 1 {
+					fmt.Println("Motion Detected")
+					led.On()
+				} else {
+					fmt.Println("No Motion Detected")
+					led.Off()
+				}
+			})
+		}
+	*/
+
 	robot := gobot.NewRobot("Goncierge",
 		[]gobot.Connection{r},
-		[]gobot.Device{pin, led},
+		//[]gobot.Device{pin, led},
+		[]gobot.Device{button, led},
 		work,
 	)
 
